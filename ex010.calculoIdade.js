5ee1a6aec4fc5afd1419589da8699e81091554d6
AlexBF_Pro <alexfirmiano2@gmail.com>
2022-07-17 16:44:50 -0300

    Criado e feito os exercícios 009 e 010.

---
+++ ex010.calculoIdade.js
@@ -0,0 +1,19 @@
+var input = require('readline-sync');
+console.log('===================');
+console.log('CÁLCULO IDADE');
+console.log('===================');
+var agora = new Date();
+var anoAtual = agora.getFullYear();
+var anoNascimento = Number(input.question('Digite seu ano de Nascimento: '));
+var idade = anoAtual - anoNascimento;
+var resultadoFinal = '';
+console.clear();
+console.log('===================');
+console.log('CÁLCULO IDADE');
+console.log('===================');
+resultadoFinal = `Sua idade é ${idade} anos `;
+if(idade >= 18) {
+  resultadoFinal += `e você atingiu a maioridade!`;
+}
+console.log(resultadoFinal);
+console.log('===================');
